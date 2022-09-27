61b51b019b97fcace4631eb3f9a741dae627dfa6
rtq998916 <rentianqi99@126.com>
2022-09-27 19:44:28 -0400

    F22-Starter-DB

--- some_columbia_student_sql.sql
+++ some_columbia_student_sql.sql
@@ -1,20 +1,6 @@
 /*
  Create table statement.
  */
-create table columbia_student
-(
-    auto_id     int auto_increment
-        primary key,
-    last_name   varchar(128) not null,
-    first_name  varchar(128) not null,
-    middle_name varchar(128) null,
-    email       varchar(256) not null,
-    uni         varchar(12)  not null,
-    constraint columbia_student_email_uindex
-        unique (email),
-    constraint columbia_student_uni_uindex
-        unique (uni)
-);
 
 /*
  Example insert statement. Note that since auto_id is auto-increment, the insert
@@ -22,4 +8,6 @@ create table columbia_student
  */
  insert into columbia_student (last_name, first_name, middle_name, email, uni)
     values ("Ferguson", "Donald", "Francis", "dff@cs.columbia.edu", "dff9");
+ insert into columbia_student (last_name, first_name, middle_name, email, uni)
+    values ("Ren", "Tianqi", "Null", "tr2669@columbia.edu", "tr2669");
 
